30eb23b5a053589c04c4537570ea7cd74de22751
윤다예 <dayeoon9012@sk.com>
2019-06-04 17:22:01 +0900

    review/ activityManagement

--- amabono-activity-management/src/main/java/com/intensive/sk/controller/ActivityManagementController.java
+++ amabono-activity-management/src/main/java/com/intensive/sk/controller/ActivityManagementController.java
@@ -3,6 +3,7 @@ package com.intensive.sk.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +12,24 @@ import com.intensive.sk.entity.Activity;
 import com.intensive.sk.entity.ActivityManagement;
 import com.intensive.sk.entity.Member;
 import com.intensive.sk.entity.Status;
+import com.intensive.sk.service.ActivityManagementService;
 
 @RestController
 public class ActivityManagementController {
 	
+	@Autowired
+	ActivityManagementService service;
+	
 	@GetMapping("/getActivityManagement")
 	public ActivityManagement activityManagement(int id) {
-		ActivityManagement result = new ActivityManagement();
+		ActivityManagement result = service.getActivityManagement(id);
 		
 		return result;
 	}
 	
 	@GetMapping("/getAllActivityManagements")
 	public List<ActivityManagement> activityManagements(){
-		List<ActivityManagement> results = new ArrayList<>();
+		List<ActivityManagement> results = service.getAllActivityManagements();
 		
 		return results;
 	}
@@ -32,10 +37,13 @@ public class ActivityManagementController {
 	@PostMapping("/apply")
 	public void apply(Activity activity, Member member) {
 		
+		service.apply(member, activity);
+		
 	}
 	
 	@PostMapping("/statusChange")
 	public void statusChange(int id, Status status) {
+		service.statusChange(id, status);
 		
 	}
 }
--- amabono-review/src/main/java/com/intensive/sk/AmabonoReviewApplication.java
+++ amabono-review/src/main/java/com/intensive/sk/AmabonoReviewApplication.java
@@ -3,6 +3,7 @@ package com.intensive.sk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AmabonoReviewApplication {
 
