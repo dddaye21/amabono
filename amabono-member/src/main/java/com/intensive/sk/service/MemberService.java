2781d51e8919459c4007459f4396e81d1f8a1cde
윤다예 <dayeoon9012@sk.com>
2019-06-04 15:21:16 +0900

    member CRUD 생성 login 제외

--- amabono-member/src/main/java/com/intensive/sk/service/MemberService.java
+++ amabono-member/src/main/java/com/intensive/sk/service/MemberService.java
@@ -1,9 +1,12 @@
 package com.intensive.sk.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.intensive.sk.entity.Member;
+import com.intensive.sk.entity.MemberType;
 import com.intensive.sk.repository.MemberRepository;
 
 @Service
@@ -16,6 +19,38 @@ public class MemberService {
 		
 		memberRepository.save(member);
 		
+	}
+	
+	public Member getMemberById(int id) {
+		
+		Member member = memberRepository.getOne(id);
+		
+		
+		return member;
+	}
+	
+	public void deleteMember(Member member) {
+		Member target = this.getMemberById(member.getId());
+		
+		memberRepository.delete(target);
+		
+	}
+	
+	
+	public void modifyMember(Member member) {
+		Member target = this.getMemberById(member.getId());
+		
+		target.setInterests(member.getInterests());
+		target.setLocation(member.getLocation());
+		target.setMajor(member.getMajor());
+		target.setPassword(member.getPassword());
+		
+		
+		memberRepository.save(target);
+	}
+	
+	public List<Member> getAllMember(){
+		return memberRepository.findAll();
 	}
 	
 	
