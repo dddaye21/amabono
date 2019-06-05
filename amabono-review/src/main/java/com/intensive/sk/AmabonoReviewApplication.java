c4f5fd580313a296066822f0635bb2e08aa87fee
SKCC\07260 <dongkyu.choi@sk.com>
2019-06-05 09:19:52 +0900

    review eureka 설정 추가

--- amabono-review/src/main/java/com/intensive/sk/AmabonoReviewApplication.java
+++ amabono-review/src/main/java/com/intensive/sk/AmabonoReviewApplication.java
@@ -2,9 +2,11 @@ package com.intensive.sk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AmabonoReviewApplication {
 
 	public static void main(String[] args) {
